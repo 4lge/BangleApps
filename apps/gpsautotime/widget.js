7551a443d69672045b5f3083974d2a4b6d761486
Albrecht Gebhardt <albrecht.gebhardt@aau.at>
2022-07-27 20:35:38 +0200

    use less space in top line

--- apps/gpsautotime/widget.js
+++ apps/gpsautotime/widget.js
@@ -21,7 +21,7 @@
 	// add your widget
 	WIDGETS["gpsAutoTime"]={
 		area:"tl", // tl (top left), tr (top right), bl (bottom left), br (bottom right)
-		width: show ? 4*6+2 : 0, // width of the widget
+		width: show ? 1*6+2 : 0, // width of the widget
 		draw: function() {
 			if (!show) return;
 			g.reset(); // reset the graphics context to defaults (color/font/etc)
@@ -30,8 +30,8 @@
 				// time is uptodate
 				g.setColor('#00ff00'); // green
 			}
-			g.drawString("auto", this.x, this.y);
-			g.drawString("time", this.x, this.y+10);
+			g.drawString("a", this.x, this.y);
+			g.drawString("t", this.x, this.y+10);
 		}
 	};
 
